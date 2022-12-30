a1d9fdbff1fa4e67d1c8c5c8eca90b48e84ce102
CORAL HSU <renchu0301@gmail.com>
2022-12-31 02:56:41 +0800

    update
    1231

---
+++ Assets/Scripts/PlayerMovemen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovemen : MonoBehaviour
+{
+    // Init
+    public float moveSpeed = 0.2f;
+
+    // Properties
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRenderer;
+
+    void Start()
+    {
+        // Animator
+        myAnimator = GetComponent<Animator>();
+
+        // Sprite
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+  
+    }
+}
